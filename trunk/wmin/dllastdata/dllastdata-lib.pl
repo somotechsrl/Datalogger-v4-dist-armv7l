66e4c5116dde343db7c66e1438167ba6cfce5db7
localuser <localuser@345a005c-e3bd-4cd1-b973-cc977f7e30e5>
2019-07-31 06:24:33 +0000

    Aggiornamento automatico da Make

--- trunk/wmin/dllastdata/dllastdata-lib.pl
+++ trunk/wmin/dllastdata/dllastdata-lib.pl
@@ -4,9 +4,15 @@ use WebminCore;
 init_config();
 
 sub  dllastdata_buttons {
-	my $fn;
+	my $fn,@fl;
 	$fn=`ls /tmp | grep .last`;
 	$fn =~ s/[.].*//g;
-	return $fn;
+	@fl = split " " $fn;	
+	
+	ui_buttons_start();
+	foreach my $button_name (@fl) {
+		ui_button('show.cgi',$button_name);
+		}
+	ui_buttons_end();
 	}
 
