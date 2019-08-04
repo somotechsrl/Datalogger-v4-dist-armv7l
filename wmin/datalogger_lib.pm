9744d949b57662f05c4b32e1465a70854fdf1038
localuser <localuser@345a005c-e3bd-4cd1-b973-cc977f7e30e5>
2019-08-04 20:11:59 +0000

    datalogger

--- wmin/datalogger_lib.pm
+++ wmin/datalogger_lib.pm
@@ -1,8 +1,11 @@
 # Converts CSV table to Columns Table Webmin
 # # Doesn't se ui_table_start, ui_table end
-sub csv2uiColumns {
+#
 
-	my ($title,$filedata) = @_;
+# Output CSV data 
+sub csvFileOut {
+
+	my ($filedata) = @_;
 
 	# this is a CSV with '|' as separator - first line is 'head'
 	my @data,@head,@type;	
@@ -11,6 +14,9 @@ sub csv2uiColumns {
 	foreach my $line (split /\n/,$filedata) {
 
 		# extracts row head and nr
+		# standard CSV received by API is:
+		# 	 separated by '|' 
+		# 	 with head/data prefix
 		my @row=split /[|]/, $line;
 		my $typ=shift @row;
 
@@ -23,11 +29,10 @@ sub csv2uiColumns {
 			}
 		}
 
-	# normalized head (language table)
+	# normalized head (from webmin language table, if any)
 	my @nhead;
 	foreach $f (@head) {push(@nhead,$text{$f} ne '' ? $text{$f} : $f);}
 
-	print &ui_table_start($title);
 	# Show the table with add links
 	print &ui_form_columns_table(
 		undef,
@@ -43,10 +48,27 @@ sub csv2uiColumns {
 		undef,
 		$text{'table_nodata'},
 		);
-	# table end
-	print &ui_table_end();
 	}
 
+
+# Generic data file output - tries to recognize format
+sub dataFileOut {
+
+	my ($title,$filedata) = @_;
+	
+	# block title
+	print &ui_table_start($title);
+
+	# check if is a 'CSV' data file or flat
+	# must contain data[|] statements
+	if($filedata =~ /[\n]?data[|]/) {
+		csvFileOut($filedata);
+	} else {
+		print "<pre>$filedata</pre>"; 
+		}
+
+	print &ui_table_end(); 
+	}
 
 return 1;
 exit;
