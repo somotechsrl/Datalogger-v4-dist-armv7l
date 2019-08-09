c736dd229a5960961dfb9db8bd2aae60374d2106
localuser <localuser@345a005c-e3bd-4cd1-b973-cc977f7e30e5>
2019-08-09 19:37:48 +0000

    Fix dlconfig

--- wmin/dlconfig/dlconfig-lib.pl
+++ wmin/dlconfig/dlconfig-lib.pl
@@ -45,7 +45,6 @@ sub dlconfig_create {
 	&dataloggerShowConfig(\@plist,"/tmp/$module.edit");
 	print &ui_table_end();
 
-	@cmdlist=[ ["command" , $text{"save_data"} ], [ "command" , $text{"cancel_data"} ]  ];
 	}
 
 sub dlconfig_display {
@@ -56,56 +55,5 @@ sub dlconfig_display {
 	$filedata=`/opt/datalogger/api/iifConfig $module print`;
 	&dataloggerCsvOut($filedata);
 	print &ui_table_end();
-
-	@cmdlist=[ [ "command" , $text{"create_data"} ], [ "command", $text{"delete_data"} ] ];
-	}
-
-# Raw data format - best to specilize
-sub dlconfig_show {
-
-	ReadParse();
-	dd %in;
-
-	# work variables
-	my $command, my $module;
-
-	# searches command and module -- priority tu submit buttons..
-	$command=$in{"command"};
-	if($in{"moduleButton"} ne "") {
-		$module=$in{"moduleButton"};
-		}
-	elsif($in{"module"} ne "") {
-		$module=$in{"module"};
-		}
-	if($in{"command"} ne "") {
-		$command=$in{"command"};
-		}
-
-	# sets form management
-	print &ui_form_start('mod_config.cgi',"POST");
-
-	# Creates new config - here to update correctly buttons.
-	if($command eq $text{"save_data"}) {
-		&dlconfig_save($module);
-		}
-	# Earse alla Config - here to update correctly buttons.
-	elsif($command eq $text{"delete_data"}) {
-		&dlconfig_delete($module);
-		}
-	elsif($command eq $text{"create_data"}) {
-		&dlconfig_create($module);
-		}
-	# default action
-	elsif($module ne "")  {
-		&dlconfig_display($module,);
-		}
-
-
-	# Active Modules
-	print &dataloggerVarHtml("moduleSelectActive",$module);	
-
-	# saves last command for re-usage
-	print ui_hidden("module",$module);
-	print ui_form_end(@cmdlist);
 	}
 
