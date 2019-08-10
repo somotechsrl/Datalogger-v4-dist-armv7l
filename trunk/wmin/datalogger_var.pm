efcd1299bbe1b2d878a461d90bf519d2896acfc2
localuser <localuser@345a005c-e3bd-4cd1-b973-cc977f7e30e5>
2019-08-10 08:49:05 +0000

    Aggiornamento automatico binari

---
+++ trunk/wmin/datalogger_var.pm
@@ -0,0 +1,129 @@
+#========================================================================
+# Needed for debug
+#========================================================================
+#use strict;
+use warnings;
+
+use WebminCore;
+use Data::Dump;  # use Data::Dumper;
+
+# define datalogger global path
+my $DLPACKAGE="/opt/datalogger";
+my $DLBWIDTH="width=16em;min-width: 16em;";
+
+#========================================================================
+# Gets  Module key by Alt Descr - AWFUL
+#========================================================================
+sub getModuleByAltDescr($descr) {
+	my ($descr) = @_;
+	my $mlist=`/opt/datalogger/iif/00list`;
+	my @md = split /[\n]/,$mlist;
+	foreach my $mo (@md) {
+		@ml = split /[|]/,$mo;
+		$alt=@ml[1]." ".@ml[2];
+		if($alt eq $descr) {
+			return @ml[5];
+			}
+		}
+	return "";
+	}
+
+#========================================================================
+# Generates Submit Buttons for Enabled Drivers
+#========================================================================
+sub  dataloggerShowSubmitModule {
+
+	my ($name,$disable) = @_;
+	
+	$fn=`ls $DLPACKAGE/etc/iif.d`;
+	@fl = split(/[ \t\n\r]/,$fn);	
+	
+	my $res=&ui_buttons_start();
+	foreach my $button_value (@fl) {
+		my $button_descr=`/opt/datalogger/api/iifAltDescr $button_value`;
+		$res.=&ui_submit($button_descr,$name,$disable, "style='$DLBWIDTH'");
+		}
+	$res.=&ui_buttons_end();
+	
+	return $res;
+	}
+
+
+#========================================================================
+# Generates Variable HTML input for  Mapped vars
+#========================================================================
+sub dataloggerVarHtml {
+
+	my ($name,$value,$disable) = @_;
+
+	# 'pause boxes
+	if($name =~ /PAUSE$/) {
+		return ui_checkbox($name,"true",undef,$value eq "true");
+		}
+	if($name =~ /FREQ$/) {
+		return ui_textbox($name,$value,5,0,5,"type='number'");
+		}
+	if($name =~ /DESCR$/) {
+		return ui_textbox($name,$value,40,0,5,"type='text'");
+		}
+	if($name eq "moduleSelectActive") {
+		$filedata=`$DLPACKAGE/api/sel/menabled`;
+		my ($rhead,$rdata)=dataloggerArrayFromCSV($filedata);
+		my @head=\@$rhead,my @options=\@$rdata;
+		return &ui_select($name,$value,@options,undef,undef,undef,undef,undef);
+		}	
+	if($name eq "moduleSubmitActive") {
+		return &dataloggerShowSubmitModule($name,$disable);
+		}	
+
+	if($name eq "moduleSelectAll") {
+		$filedata=`$DLPACKAGE/api/sel/module`;
+		my ($rhead,$rdata)=dataloggerArrayFromCSV($filedata);
+		my @head=\@$rhead,my @options=\@$rdata;
+		return &ui_select($name,$value,@options,undef,undef,undef,undef,undef);
+		}
+
+	if($name eq "schannel" || $name eq "mbserial") {
+		$filedata=`$DLPACKAGE/api/sel/mbserial`;
+		my ($rhead,$rdata)=dataloggerArrayFromCSV($filedata);
+		my @head=\@$rhead,my @options=\@$rdata;
+		return &ui_select($name,$value,@options,undef,undef,undef,undef,undef);
+		}
+
+	if($name eq "mbchannel") {
+		my $combos;
+		$combos=&dataloggerVarHtml("mbserial",$value);
+		$combos.=&dataloggerVarHtml("mbserial",$value);
+		$combos.=&dataloggerVarHtml("mbserial",$value);
+		return $combos;
+		}
+
+	if($name eq "mbaddress") {
+		$filedata=`$DLPACKAGE/api/sel/mbaddress`;
+		my ($rhead,$rdata)=dataloggerArrayFromCSV($filedata);
+		my @head=\@$rhead,my @options=\@$rdata;
+		return &ui_select($name,$value,@options,undef,undef,undef,undef,undef);
+		}
+
+	if($name eq "mbdelay") {
+		if(!$value) {$value=500};
+		return ui_textbox($name,$value,5,0,5,"type='number'");
+		}
+
+	if($name eq "mboffset") {
+		if(!$value) {$value=0};
+		return ui_textbox($name,$value,5,0,5,"type='number'");
+		}
+
+	if($name =~ "hidden_") {
+		@nn=split "_",$name;
+		$xname=@nn[1];
+		return &ui_hidden($xname,$value);
+		}	
+
+		
+	# default
+	return ui_textbox($name,$value,40,0,undef,undef);
+	return $selectValue;
+	}
+
